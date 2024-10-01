663b58b7629f33718d24e9ae0be7853716f74dd5
johnLamberts <faithsacredoo3@gmail.com>
2024-10-01 17:31:42 +0800

    feat: add museums page

--- src/components/museum.tsx
+++ src/components/museum.tsx
@@ -10,7 +10,7 @@ interface Museumrops {
 
 const Museum = ({ icon, title, description,  address , button}: Museumrops) => {
   return (
-    <div className="flex flex-row md:gap-20 gap-6 md:p-10 p-8 border border-primary-100">
+    <div className="flex lg:flex-row flex-col md:gap-20 gap-6 md:p-10 p-8 border border-primary-100">
       {icon &&  <img src={icon} width={"50%"} height={"auto"} alt={title} />}
       <div className="flex flex-col gap-4">
         <p className="font-display md:text-display-md text-display-sm font-normal text-[#0B0400]">
