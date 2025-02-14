1f7d9a5b04c8a5fd6e0f9be6702656d10f4704de
johnLamb3rts <asisjohnlambert5@gmail.com>
2025-02-14 13:48:40 +0800

    fix: no enum for events module

--- src/lib/enum.ts
+++ src/lib/enum.ts
@@ -4,7 +4,7 @@ export enum CMS_KEYZ{
   READ_CREATE_USERS_ADDED = 'users',
   READ_CREATE_VISITORS_ADDED = 'visitors',
   READ_CREATE_MUSEUMS_ADDED = 'museums',
-
+  READ_CREATE_EVENTS_ADDED = 'events',
 
   LATEST_FEATURED_EVENTS = "featured_events",
 
